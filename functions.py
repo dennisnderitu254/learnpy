420e1814a0170874fe10b02ddbc4f7546301e3a3
Dennis Nderitu <dknderitu@gmail.com>
2017-06-20 15:11:38 +0300

    working with functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+# this is a function called cube that takes an arguement called number
+
+def cube(number):
+   
+    return number**3
+
+def by_three(number):
+    if number % 3 == 0:
+       return cube(number)
+    else:
+       return False
+    
